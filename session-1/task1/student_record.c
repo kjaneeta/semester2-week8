2a77b975058778d2b0d095f04fe4f7a088ca4d64
kjaneeta <jzps0355@leeds.ac.uk>
2026-03-24 20:35:53 +0000

    session 1 task 1 done

--- session-1/task1/student_record.c
+++ session-1/task1/student_record.c
@@ -19,8 +19,17 @@
 float calculate_average(struct student_record student) {
     // TODO: Implement this function to calculate and return the average
     // of the three module marks
+
     
-    return 0.0; // Placeholder return value
+    float sum = 0;
+    for(int i = 0; i < 3; i++)
+    {
+        sum = sum + student.marks[i];
+    }
+
+    float avg = sum/3;
+
+    return avg; // Placeholder return value
 }
 
 /**
@@ -31,6 +40,17 @@ float calculate_average(struct student_record student) {
 void display_student(struct student_record student) {
     // TODO: Implement this function to display student information
     // Format: Name, ID, individual marks, and average mark
+
+    printf("Name: %s, ID: %d, Marks: ", student.name, student.id);
+
+    for (int i = 0; i < 3; i++) 
+    {
+        printf("%f ", student.marks[i]);
+    }
+
+    printf(", Average: %f\n", calculate_average(student));
+
+
 }
 
 /*
@@ -40,9 +60,19 @@ int main( void ) {
     // TODO: Create and initialise a student variable with sample data
     // Example: name = "John Smith", id = 12345, marks = {75.0, 68.5, 81.0}
     
-    struct student_record student;
+    struct student_record student = {"John Smith", 12345, {75.0f, 68.5f, 81.0f}};
     
     // TODO: display the student data and average mark
-    
+    display_student(student);
+    printf("Average = %f\n", calculate_average(student));
+
+    // Modify the student's name and third module mark
+    strcpy(student.name, "Jane Doe"); // strcpy -> copy one string into another
+    student.marks[2] = 85.0f;
+
+    // Display updated student
+    display_student(student);
+    printf("Average = %f\n", calculate_average(student));
+
     return 0;
 }
